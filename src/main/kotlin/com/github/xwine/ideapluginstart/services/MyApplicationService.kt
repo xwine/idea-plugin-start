f0ded9311257fab3df6c01ffce721343f2f7d956
GitHub Action <action@github.com>
2022-03-30 13:46:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/xwine/ideapluginstart/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xwine/ideapluginstart/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xwine.ideapluginstart.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xwine.ideapluginstart.MyBundle
 
 class MyApplicationService {
 
